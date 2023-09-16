5a1c44a3e9abf63eab3fbd8a259ab8f5ecaca8ed
Runneso <maks.safronov.2006@gmail.com>
2023-09-17 00:24:39 +0300

    1.1 (release)

--- keyboards.py
+++ keyboards.py
@@ -10,8 +10,6 @@ remove_task_button = types.KeyboardButton(text="➖ Remove task.")
 change_timezone_button = types.KeyboardButton(text="🕐 Change timezone task.")
 main_keyboard.keyboard.extend([[add_task_button, remove_task_button, change_timezone_button]])
 
-
-
 change_timezone_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[])
 UTC = [['UTC +12'], ['UTC +11'], ['UTC +10'], ['UTC +9'], ['UTC +8'], ['UTC +7'], ['UTC +6'], ['UTC +5'], ['UTC +4'],
        ['UTC +3'], ['UTC +2'], ['UTC +1'], ['UTC 0'], ['UTC -1,'], ['UTC -2'], ['UTC -3'], ['UTC -4'], ['UTC -5'],
